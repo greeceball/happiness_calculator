1206ec15030bb40fb3baf046d4bac43b53fe6635
Colby Harris <griff33@gmail.com>
2020-03-03 11:03:51 -0700

    Updated EntryListTableViewController

---
+++ HappinessCalculator/HappinessCalculator/Controllers/EntryTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  EntryTableViewCell.swift
+//  NotificationPatternsJournal
+//
+//  Created by Colby Harris on 3/3/20.
+//  Copyright © 2020 Trevor Walker. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var higherOrLowerLabel: UILabel!
+    @IBOutlet weak var isEnabledSwitch: UISwitch!
+    
+    //MARK: - Properties
+    var entry: Entry?
+    
+    //MARK: - Helper Functions
+    func setEntry(entry: Entry, averageHappiness: Int){
+        self.entry = entry
+    }
+
+    func updateUI(averageHappiness: Int){
+        guard let entry = entry else {return}
+        titleLabel.text = entry.title
+        isEnabledSwitch.isOn = entry.isIncluded
+        
+        // Update HigherOrLowerLabel after notifications
+        
+    }
+    
+    
+}
